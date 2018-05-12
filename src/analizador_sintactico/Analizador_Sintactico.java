f7135382e76bd43e68d40cd0b87b31646c4a9f1f
Ana Victoria Cavazos Argot <anavca@outlook.com>
2018-05-12 00:32:25 -0500

    Sigo trabajando :v

--- src/analizador_sintactico/Analizador_Sintactico.java
+++ src/analizador_sintactico/Analizador_Sintactico.java
@@ -12,45 +12,106 @@ public class Analizador_Sintactico {
 
 	public void analizar(List<Token> tokens) {
 		i = 0;
-		r_starto(tokens);
-		// System.out.println(iterator.next().getValor());
+		r_S_starto(tokens);
 	}
 
-	public void r_starto(List<Token> tokens) {
-		if (tokens.get(i).getValor().equals("starto")) {
-			System.out.print("starto");
+	public void r_S_starto(List<Token> tokens) {
+		if (tokens.get(i).getTipo().equals("INICIO")) {
+			System.out.println("starto");
 			i++;
-			if (tokens.get(i).getValor().equals("(")) {
-				System.out.print("(");
-				i++;
-				if (tokens.get(i).getValor().equals(")")) {
-					System.out.print(")");
-					i++;
-					if (tokens.get(i).getValor().equals("{")) {
-						System.out.println("{");
-						i++;
-						// CUERPO_STARTO
-						if (tokens.get(i).getValor().equals("}")) {
-							System.out.println("}");
-							i++;
-						} else {
-							System.out.println("ERROR: Se esperaba } | Token recibido: "+tokens.get(i).getValor());
-						}
-					} else {
-						System.out.println("ERROR: Se esperaba { | Token recibido: "+tokens.get(i).getValor());
-					}
-				} else {
-					System.out.println("ERROR: Se esperaba ) | Token recibido: "+tokens.get(i).getValor());
-				}
-			} else {
-				System.out.println("ERROR: Se esperaba ( | Token recibido: "+tokens.get(i).getValor());
-			}
 		} else {
-			System.out.println("ERROR: Se esperaba starto | Token recibido: "+tokens.get(i).getValor());
+			System.out.println("ERROR: Se esperaba starto | Token recibido: " + tokens.get(i).getValor());
+		}
+		if (tokens.get(i).getTipo().equals("PARENTESIS_APERTURA")) {
+			System.out.println("(");
+			i++;
+		} else {
+			System.out.println("ERROR: Se esperaba ( | Token recibido: " + tokens.get(i).getValor());
+		}
+		if (tokens.get(i).getTipo().equals("PARENTESIS_CERRADURA")) {
+			System.out.println(")");
+			i++;
+		} else {
+			System.out.println("ERROR: Se esperaba ) | Token recibido: " + tokens.get(i).getValor());
+		}
+		if (tokens.get(i).getTipo().equals("INICIO_BLOQUE")) {
+			System.out.println("{");
+			i++;
+		} else {
+			System.out.println("ERROR: Se esperaba { | Token recibido: " + tokens.get(i).getValor());
+		}
+		// CUERPO_STARTO
+		r_cuerpo_starto(tokens);
+		if (tokens.get(i).getTipo().equals("FIN_BLOQUE")) {
+			System.out.println("}");
+			i++;
+		} else {
+			System.out.println("ERROR: Se esperaba } | Token recibido: " + tokens.get(i).getValor());
+		}
+
+	}
+
+	public void r_cuerpo_starto(List<Token> tokens) {
+
+		String tipo = tokens.get(i).getTipo();
+
+		switch (tipo) {
+		case "COMENTARIO":
+			r_Comentario(tokens);
+			break;
+		case "IMPRIMIR":
+			r_S_outo(tokens);
+			break;
+		}
+
+		// r_Declaracion
+
+		// r_S_If
+
+		// r_S_From
+
+		// Vacio
+
+	}
+
+	public void r_Comentario(List<Token> tokens) {
+		if (tokens.get(i).getTipo().equals("COMENTARIO")) {
+			System.out.println(tokens.get(i).getValor());
+			i++;
+			r_cuerpo_starto(tokens);
+		} else {
+			System.out.println("ERROR: Se esperaba # | Token recibido: " + tokens.get(i).getValor());
+			r_cuerpo_starto(tokens);
 		}
 	}
 
-	public void r_cuerpo_starto() {
+	public void r_S_outo(List<Token> tokens) {
+		if (tokens.get(i).getTipo().equals("IMPRIMIR")) {
+			System.out.println(tokens.get(i).getValor());
+			i++;
+			if (tokens.get(i).getTipo().equals("PARENTESIS_APERTURA")) {
+				System.out.println(tokens.get(i).getValor());
+				i++;
+				// CUERPO_OUTO
+				if (tokens.get(i).getTipo().equals("PARENTESIS_CERRADURA")) {
+					System.out.println(tokens.get(i).getValor());
+					i++;
+					if (tokens.get(i).getTipo().equals("FIN_SENTENCIA")) {
+						System.out.println(tokens.get(i).getValor());
+						i++;
+					} else {
+						System.out.println("ERROR: Se esperaba ; | Token recibido: " + tokens.get(i).getValor());
+					}
+				} else {
+					System.out.println("ERROR: Se esperaba ) | Token recibido: " + tokens.get(i).getValor());
+				}
+			} else {
+				System.out.println("ERROR: Se esperaba ( | Token recibido: " + tokens.get(i).getValor());
 
+			}
+		} else {
+			System.out.println("ERROR: Se esperaba outo | Token recibido: " + tokens.get(i).getValor());
+
+		}
 	}
 }
