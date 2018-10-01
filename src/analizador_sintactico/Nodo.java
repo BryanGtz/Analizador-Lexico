3091a0001b6d2c67777529042de8c355b013ef5a
HP <HP@DESKTOP-LAI1VC7>
2018-10-01 12:29:21 -0500

    Inicio de analizador semantico

---
+++ src/analizador_sintactico/Nodo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analizador_sintactico;
+
+import java.util.ArrayList;
+import analizador_lexico.Token;
+
+/**
+ *
+ * @author User
+ */
+public class Nodo {
+    ArrayList<Nodo> hijos;
+    String nombre;
+    Token token;
+    boolean esTerminal;
+    public Nodo(){
+        
+    }
+    
+}
